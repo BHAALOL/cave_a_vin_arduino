3ad7e20004b6f77738fee375c304085a7e360b4f
Guillaume <guillaume@github.com>
2022-10-06 13:53:28 +0200

    Add File

---
+++ Arduino_Cave_a_Vin.ino
@@ -0,0 +1,81 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+int timer;
+bool etatVentilo = false;
+bool etatCompresseur = false; 
+//String dhtTemp = String(dht.readTemperature());
+//String dhtHum = String(dht.readHumidity());
+//int temp = dhtTemp.toInt();
+//int hum = dhtHum.toInt();
+//bool etatVentilo = false;
+//bool etatCompresseur = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() { 
+String dhtTemp = String(dht.readTemperature());
+String dhtHum = String(dht.readHumidity());
+int temp = dhtTemp.toInt();
+int hum = dhtHum.toInt();
+
+
+
+  if (etatCompresseur == 1){
+    Serial.print ("Compresseur allumé il fait: ");
+    Serial.print (temp);
+    Serial.print ("°C");
+    Serial.println("");
+    digitalWrite(11, HIGH);
+    }
+    else {
+      Serial.print ("Compresseur off il fait:");
+      Serial.print(temp);
+      Serial.print ("°C");
+      Serial.println("");
+      digitalWrite(11, LOW);
+      }
+       
+  if (temp >= 16){ // Activation du compresseur si au dessus de 16
+    Serial.println("Valeur du BOOL  1");
+    Serial.print("Temperature actuelle : ");
+    Serial.print(temp);
+    Serial.print("°C");
+    Serial.println("");
+    etatCompresseur = true;
+  }
+  else{
+    Serial.println("valeur du BOOL  0");
+    Serial.print("Temperature actuelle : ");
+    Serial.print(temp);
+    Serial.print("°C");
+    Serial.println("");
+    etatCompresseur = false;
+    }
+    
+  if (timer >= 2400){ // 40 Min
+      Serial.println ("Ventilo ON");
+      digitalWrite(12, HIGH);
+    }
+  else {
+    Serial.println("Ventilateur OFF");
+    digitalWrite(12, LOW);
+    }
+  if (timer >= 3600){ // 1H 
+    timer = 0;
+    Serial.println("RAZ Timer");
+    }
+  Serial.print("timer :");
+  Serial.print(timer);
+  Serial.print(" Secondes");
+  Serial.println("");
+  delay(10000);
+  timer = timer + 10 ;
+}
